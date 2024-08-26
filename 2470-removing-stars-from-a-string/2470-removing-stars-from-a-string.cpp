4c76947556f5b51db511f350a2ac56542fc6459d
Tanish Valesha <76911714+TanishValesha@users.noreply.github.com>
2024-08-26 18:54:24 +0530

    Time: 88 ms (44.99%), Space: 28.5 MB (38.35%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     string removeStars(string s) {
         stack<char> st;
-        string ans;
-        for(char ch: s){
+     for(char ch: s){
             if(ch == '*'){
                 st.pop();
             } else {
                 st.push(ch);
             }
         }
+        string ans = "";
         while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
